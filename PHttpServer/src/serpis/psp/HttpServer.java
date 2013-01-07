cd97eb446faa4faff7282e87ff85d2277253a364
Javiersanchez <javiersanchezflores17@gmail.com>
2013-01-07 09:32:12 +0100

    añadiendo HttpServer

--- PHttpServer/src/serpis/psp/HttpServer.java
+++ PHttpServer/src/serpis/psp/HttpServer.java
@@ -1,13 +1,71 @@
 package serpis.psp;
 
-public class HttpServer {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//VERSIÓN MONOHILO
 
+public class HttpServer 
+{
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+	final String newLine = "\r\n";
+	final int port = 8080;
+	final String fileNameError404 = "fileError404.html";
+	final String response200 = "HTTP/1.0 200 OK";
+	final String response404 = "HTTP/1.0 404 Not Found";
+	
+	
+	ServerSocket serverSocket = new ServerSocket (port);
+	
+	//Que el servidor se quede escuchando
+	Socket socket = serverSocket.accept();
+	
+	Scanner scanner = new Scanner (socket.getInputStream() );
+	
+	String fileName = "index.html";
+	
+	while(true){
+		String line = scanner.nextLine();
+		System.out.println(line);
+		if (line.equals(""))
+			break;
 	}
 
+	File file = new File(fileName);
+	
+	String responseFileName = file.exists() ? fileName : fileNameError404;
+	String response = file.exists() ? response200 : response404;
+	
+	String header = response + newLine + newLine;
+	byte[] headerBuffer = header.getBytes();
+	
+	OutputStream outputStream = socket.getOutputStream();
+	outputStream.write(headerBuffer);
+	
+	final int bufferSize = 2048;
+	byte[] buffer = new byte[bufferSize];
+	
+	FileInputStream fileInputStream = new FileInputStream(responseFileName);
+	
+	int count;
+	
+	while ((count = fileInputStream.read(buffer)) != -1){
+		//Retardo
+		Thread.sleep (3000); 
+		outputStream.write(buffer, 0, count);
+	
+	fileInputStream.close();
+	
+	socket.close();
+	
+	serverSocket.close();
+	}
+	}
 }
