c2a573f9b5150b7891702833fb5f9d5b8efbac46
Javiersanchez <javiersanchezflores17@gmail.com>
2013-01-25 08:27:46 +0100

    HtpServer Version Monohilo 2

--- PHttpServer/src/serpis/psp/HttpServer.java
+++ PHttpServer/src/serpis/psp/HttpServer.java
@@ -2,54 +2,75 @@ package serpis.psp;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-//VERSIÓN MONOHILO
+//VERSIÓN MONOHILO 2
 
 public class HttpServer 
 {
+	private static final String newLine = "\r\n";
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
-		final String newLine = "\r\n";
+		//final String newLine = "\r\n";
 		final int port = 8080;
-		final String fileNameError404 = "fileError404.html";
-		final String response200 = "HTTP/1.0 200 OK";
-		final String response404 = "HTTP/1.0 404 Not Found";
 		
+
 		ServerSocket serverSocket = new ServerSocket (port);
 		
 		while (true) {
 			//Que el servidor se quede escuchando
 			Socket socket = serverSocket.accept();
 			
+			String fileName = getFileName(socket.getInputStream());
+			writeHeader(socket.getOutputStream(), fileName);
+			writeFile(socket.getOutputStream(), fileName);
+			socket.close();
 			
-			Scanner scanner = new Scanner (socket.getInputStream() );
+		}
+		//serverSocket.close();
+	}
+	
+		private static String getFileName(InputStream inputStream){
+			Scanner scanner = new Scanner (inputStream);
 			
 			String fileName = "index.html";
 			
+			//Linea GET
 			while(true){
 				String line = scanner.nextLine();
 				System.out.println(line);
 				if (line.equals(""))
 					break;
 			}
+			return fileName;		
+		}
 		
-			File file = new File(fileName);
-			
-			String responseFileName = file.exists() ? fileName : fileNameError404;
+		private static void writeHeader(OutputStream outputStream, String fileName) throws IOException{
+			final String response200 = "HTTP/1.0 200 OK";
+			final String response404 = "HTTP/1.0 404 Not Found";
+			File file = new File (fileName);
 			String response = file.exists() ? response200 : response404;
-			
 			String header = response + newLine + newLine;
 			byte[] headerBuffer = header.getBytes();
 			
-			OutputStream outputStream = socket.getOutputStream();
 			outputStream.write(headerBuffer);
+		}
+		
+		private static void writeFile(OutputStream outputStream, String fileName) throws IOException, InterruptedException{
+			final String fileNameError404 = "fileError404.html";
 			
+			File file = new File(fileName);
+			String responseFileName = file.exists() ? fileName : fileNameError404;
+			
+			//Hace falta una linea en blanco, ya que lo dice el protocolo HTTP
+
 			final int bufferSize = 2048;
 			byte[] buffer = new byte[bufferSize];
 			
@@ -62,9 +83,7 @@ public class HttpServer
 				Thread.sleep (3000); 
 				outputStream.write(buffer, 0, count);
 			}
-			fileInputStream.close();
 			
-			socket.close();
+			fileInputStream.close();
 		}
-	}
 }
